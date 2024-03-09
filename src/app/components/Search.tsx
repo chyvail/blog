988120914d0be92fde47d39caf87482ee61ac08d
joshua chivile <joshuachivile@gmail.com>
2024-03-09 23:53:42 +0300

    Search functionality working

--- src/app/components/Search.tsx
+++ src/app/components/Search.tsx
@@ -1,16 +1,22 @@
-import React from "react";
+import Link from "next/link";
+import React, { useState } from "react";
 import { CiSearch } from "react-icons/ci";
 
 export default function Search() {
+  const [search, setSearch] = useState("");
   return (
     <div className="relative">
       <input
         type="text"
-        className="mx-4 px-5 py-1 rounded"
-        placeholder="Search"
+        className="mx-4 px-5 py-1 rounded text-slate-900"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        placeholder="Text"
       />
-      <div className="absolute inset-y-0 right-0 pr-7 flex items-center pointer-events-none">
-        <CiSearch className="text-slate-900" />
+      <div className="absolute inset-y-0 right-0 pr-7 flex items-center">
+        <Link href={`/search/${search}`}>
+          <CiSearch className="text-slate-900 cursor-pointer pointer-events-none" />
+        </Link>
       </div>
     </div>
   );
---
+++ src/app/search/[text]/page.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import Heading from "../../components/Heading";
+import blogData from "@/app/data/blogData";
+import Link from "next/link";
+
+interface SearchProps {
+  params: {
+    text: string;
+  };
+}
+
+const SearchPage: React.FC<SearchProps> = ({ params }) => {
+  const { text } = params;
+
+  const filteredPosts = blogData.filter((posts) =>
+    posts.title.toLowerCase().includes(text.toLocaleLowerCase())
+  );
+
+  console.log(filteredPosts);
+
+  return (
+    <div className="text-center">
+      <Heading title="Search Blogs" />
+      <div>
+        <h6 className="font-normal mt-4 text-gray-600 text-md">
+          Showing {filteredPosts.length | 0} results of {text}
+        </h6>
+      </div>
+      <div className="custom-container mx-auto mt-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
+          {filteredPosts &&
+            filteredPosts.map((post) => (
+              <div className="blog" key={post.id}>
+                <img
+                  src={post.image}
+                  alt={post.slug}
+                  className="object-cover h-auto w-full"
+                />
+                <div>
+                  <h6 className="font-normal mt-4 flex justify-between text-gray-600 text-sm">
+                    <div>{post.date}</div>
+                    <div>5 min Read</div>
+                  </h6>
+                  <div className="mt-2">
+                    <Link href={`/blog/${post.slug}`}>
+                      <h4 className="text-2xl font-extrabold text-slate-900 post-title">
+                        {post.title}
+                      </h4>
+                    </Link>
+                    <p className="font-normal mt-2 flex justify-between text-gray-600 text-base post-description">
+                      {post.content}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SearchPage;
