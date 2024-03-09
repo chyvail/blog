7b993d0fdb10a32e9a96bcd8976f3ea755ddbeb6
joshua chivile <joshuachivile@gmail.com>
2024-03-09 17:49:38 +0300

    Added Nav and Footer to layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import Nav from "./components/Nav";
+import BlogList from "./components/BlogList";
 
 export default function Home() {
   return (
     <main>
-      Initial Page
+      <BlogList />
     </main>
   );
 }
